93df62eb84c5a66571efd89c978ee194fc6dc5aa
fshabanov <fshabanov01@gmail.com>
2022-07-10 23:04:47 +0400

    update app Port

--- build/config.js
+++ build/config.js
@@ -5,6 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PORT = exports.HTML_FILES_PATH = exports.STATIC_PATH = void 0;
 const path_1 = __importDefault(require("path"));
-exports.STATIC_PATH = path_1.default.join(__dirname, '..', 'public');
-exports.HTML_FILES_PATH = path_1.default.join(exports.STATIC_PATH, 'html');
-exports.PORT = 3002;
+exports.STATIC_PATH = path_1.default.join(__dirname, "..", "public");
+exports.HTML_FILES_PATH = path_1.default.join(exports.STATIC_PATH, "html");
+exports.PORT = process.env.PORT || 3002;
